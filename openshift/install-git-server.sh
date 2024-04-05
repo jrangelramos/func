c94befecb41b402687dd49d0af0c3e1ee6aa16ec
Jefferson Ramos <jeramos@redhat.com>
2024-04-04 22:14:48 -0300

    making test GitServer install with plain file (no STDIN)

--- openshift/install-git-server.sh
+++ openshift/install-git-server.sh
@@ -12,42 +12,12 @@ GITSERVER_IMAGE="${GITSERVER_IMAGE:-ghcr.io/jrangelramos/gitserver-unpriv:latest
 go env
 source "$(go run knative.dev/hack/cmd/script e2e-tests.sh)"
 
+BASEDIR=$(dirname "$0")
+
 header "Installing Test GitServer"
-
-echo "Current project namespace"
-oc project
-cat << EOF | oc apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  labels:
-    app: gitserver
-  name: gitserver
-spec:
-  containers:
-  - image: ${GITSERVER_IMAGE}
-    name: user-container
-    ports:
-      - containerPort: 8080
-EOF
-echo "POD CREATED"
+sed "s!_GITSERVER_IMAGE_!${GITSERVER_IMAGE}!g" "${BASEDIR}/openshift/deploy/gitserver-service.yaml" > "${BASEDIR}/openshift/deploy/gitserver.yaml"
+oc apply -f opensgift/deploy/gitserver.yaml
 oc wait pod/gitserver --for=condition=Ready --timeout=15s
-
-
-cat << EOF | oc apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: gitserver
-spec:
-  type: NodePort
-  selector:
-    app: gitserver
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 8080
-EOF
 
 subheader "Exposing Test GitServer route"
 oc expose service gitserver --name=gitserver --port=8080
